280865c83838934c7354ed07d3c2bf7d485bb665
Muhamamad Aris <mhdarisid@gmail.com>
2023-11-19 21:24:40 +0700

    Cara 2

---
+++ application/views/about.php
@@ -0,0 +1,16 @@
+
+<div class="container mt-5">
+    <h1>About Us</h1>
+    <p>Welcome to our website! This is the about page where you can learn more about our organization.</p>
+
+    <div class="row mt-4">
+        <div class="col-md-6">
+            <h2>Our Mission</h2>
+            <p>We are committed to providing high-quality services to our clients and making a positive impact on the community.</p>
+        </div>
+        <div class="col-md-6">
+            <h2>Our Team</h2>
+            <p>Meet our dedicated team of professionals who work tirelessly to achieve our goals.</p>
+        </div>
+    </div>
+</div>
