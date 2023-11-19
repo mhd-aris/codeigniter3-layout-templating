dca8bcaad373113b3a56540f83416c703792fb82
Muhamamad Aris <mhdarisid@gmail.com>
2023-11-19 21:35:00 +0700

    Cara kerja library (Mama_hari)

---
+++ application/libraries/Nama_hari.php
@@ -0,0 +1,9 @@
+<?php
+class nama_hari{
+    function hari_ini(){
+        echo "Hari ini adalah selasa !";
+    }
+    function sekarang($nama_hari){
+        echo "Nama hari ini adalah ". $nama_hari ." !";
+    }
+}
